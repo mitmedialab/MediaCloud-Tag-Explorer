143e1d880cc0879f10a894fc6e7515f7714cbb02
Rahul Bhargava <rahulb@media.mit.edu>
2013-06-13 15:22:16 -0400

    realtime and readability examples work, better support code structure, ignore log and local config

--- example-readability.py
+++ example-readability.py
@@ -4,15 +4,14 @@ import ConfigParser
 import json
 from pubsub import pub
 import nltk
-
-from readability.readabilitytests import ReadabilityTool
+import mcexamples.algorithms
 
 from mediacloud.api import MediaCloud
 from mediacloud.storage import *
-import mediacloud.examples
+import mcexamples.algorithms
 
 '''
-This example file fetches the latest 25 stories from MediaCloud and saves their metadata 
+This example file fetches the latest 20 stories from MediaCloud and saves their metadata 
 to a 'mediacloud' database.  It adds in the computed Flesh-Kindcaid reading grade Level to 
 the story pre-save event subscription.
 '''
@@ -31,38 +30,8 @@ mc = MediaCloud( config.get('api','user'), config.get('api','pass') )
 results = mc.allProcessed()
 print "Fetched "+str(len(results))+" stories"
 
-def addFleshKincaidGradeLevelToStory(db_story, raw_story):
-    '''
-    Simple hook to add the Flesch-Kincaid Grade to the database.  This uses a pre-save
-    callback to add a new 'fk_grade_level' column. This relies on patched ntlk_contrib
-    code, stored in the readability module (because their published code don't work!)
-    '''
-    text = raw_story['story_text']
-    r = ReadabilityTool()
-    gradeLevel = None
-    if isEnglish(text):
-        try:
-            if (text!=None) and (len(text)>0) :
-                gradeLevel = r.FleschKincaidGradeLevel(text.encode('utf-8'))
-        except (KeyError, UnicodeDecodeError):
-            pass
-    if (gradeLevel != None):
-        db_story['fk_grade_level'] = gradeLevel
-
-def isEnglish(text):
-    '''
-    A simple hack to detect if an article is in english or not.
-    See http://www.algorithm.co.il/blogs/programming/python/cheap-language-detection-nltk/
-    '''
-    matchesEnglish = False
-    if (text!=None) and (len(text)>0) :
-        text = text.lower()
-        words = set(nltk.wordpunct_tokenize(text))
-        matchesEnglish = len(words & ENGLISH_STOPWORDS) > 0
-    return matchesEnglish
-
 # set up my callback function that adds the Flesh-Kincaid reading grade level
-pub.subscribe(addFleshKincaidGradeLevelToStory, StoryDatabase.EVENT_PRE_STORY_SAVE)
+pub.subscribe(mcexamples.algorithms.addReadingLevelToStory, StoryDatabase.EVENT_PRE_STORY_SAVE)
 
 # save all the stories in the db (this will fire the callback above)
 saved = 0
---
+++ example-realtime.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+import time
+start_time = time.time()
+
+import ConfigParser
+import json
+from pubsub import pub
+import nltk
+import logging
+
+from mediacloud.api import MediaCloud
+from mediacloud.storage import *
+import mcexamples.algorithms
+
+'''
+This example is meant to be run from a cron job on a server.  It fetches all stories 
+created after the latest one it has in it's db.  It saves the metadata for all those to 
+a 'mediacloud' database.
+'''
+
+MAX_PAGES_TO_FETCH = 5
+
+config = ConfigParser.ConfigParser()
+config.read('mc-client.config')
+
+# setup logging
+logging.basicConfig(filename='mc-realtime.log',level=logging.DEBUG)
+log = logging.getLogger('mc-realtime')
+log.info("---------------------------------------------------------------------------")
+
+# setup a connection to a local DB
+#db = CouchStoryDatabase('mediacloud', config.get('db','host'), config.get('db','port') )
+db = MongoStoryDatabase('mediacloud')
+
+# setup the mediacloud connection
+mc = MediaCloud( config.get('api','user'), config.get('api','pass') )
+
+max_story_id = db.getMaxStoryId()
+results = mc.storiesSince( max_story_id )
+
+# set up my callback function that adds the reading grade level to the story
+pub.subscribe(mcexamples.algorithms.addReadingLevelToStory, StoryDatabase.EVENT_PRE_STORY_SAVE)
+
+# save all the stories in the db (this will fire the callback above)
+saved = 0
+for page in xrange(MAX_PAGES_TO_FETCH):
+  query_page = page+1
+  results = mc.allProcessed(query_page)
+  page_has_existing_stories = False
+  log.info("Fetched "+str(len(results))+" stories (page "+str(query_page)+")")
+  for story in results:
+    if int(story['stories_id']) < int(max_story_id):
+      log.warning("  Story id "+str(story['stories_id'])+" is lower than max id ("+str(max_story_id)+")!"); 
+      log.info("  Stopping on page "+str(query_page)+" because found a story lower than existing max id")
+      page_has_existing_stories = True
+      break
+    else:
+      worked = db.addStory(story)
+      if worked:
+        saved = saved + 1
+      else:
+        log.warning("  unable to save story "+str(story['stories_id']))
+  if page_has_existing_stories:
+    break # bail once we start getting stories we should have already
+
+max_story_id = db.getMaxStoryId()
+
+end_time = time.time()
+duration = end_time - start_time
+log.info("Saved "+str(saved)+" stories - new max id "+str(max_story_id))
+if saved > 0:
+  log.info("  took "+str(duration)+" secs ("+str(round(duration/saved,2))+" secs per story)")
