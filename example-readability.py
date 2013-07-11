77b07be1a865641ed81b38f6c35d69e9eebd4fc0
Rahul Bhargava <rahulb@media.mit.edu>
2013-07-11 13:02:42 -0400

    tab cleanup and support connection settings from config file

--- example-readability.py
+++ example-readability.py
@@ -22,8 +22,7 @@ config = ConfigParser.ConfigParser()
 config.read('mc-client.config')
 
 # set up a connection to a local DB
-# db = CouchStoryDatabase('mediacloud', config.get('db','host'), config.get('db','port') )
-db = MongoStoryDatabase('mediacloud')
+db = MongoStoryDatabase(config.get('db','name'),config.get('db','host'),config.get('db','port'))
 
 # connect to MC and fetch some articles
 mc = MediaCloud( config.get('api','user'), config.get('api','pass') )
--- example-realtime.py
+++ example-realtime.py
@@ -7,14 +7,16 @@ import json
 from pubsub import pub
 import nltk
 import logging
+import sys
+import pymongo
 
 from mediacloud.api import MediaCloud
 from mediacloud.storage import *
 import mcexamples.algorithms
 
 '''
-This example is meant to be run from a cron job on a server.  It fetches all stories 
-created page by page.  It saves the metadata for all those to a 'mediacloud' database.
+This example is meant to be run from a cron job on a server.    It fetches all stories 
+created page by page.    It saves the metadata for all those to a 'mediacloud' database.
 '''
 
 MAX_PAGES_TO_FETCH = 5
@@ -28,13 +30,16 @@ logging.basicConfig(filename='mc-realtime.log',level=logging.DEBUG)
 log = logging.getLogger('mc-realtime')
 log.info("---------------------------------------------------------------------------")
 
-# setup a connection to a local DB
-db = MongoStoryDatabase('mediacloud')
+# setup a connection to the DB
+try:
+    db = MongoStoryDatabase(config.get('db','name'),config.get('db','host'),int(config.get('db','port')))
+except pymongo.errors.ConnectionFailure, e:
+    log.error(e)
+    sys.exit()
+log.info("Connected to "+config.get('db','name')+" on "+config.get('db','host')+":"+str(config.get('db','port')))
 
 # setup the mediacloud connection
 mc = MediaCloud( config.get('api','user'), config.get('api','pass') )
-
-max_story_id = db.getMaxStoryId()
 
 # set up my callback function that adds the reading grade level to the story
 pub.subscribe(mcexamples.algorithms.addReadingLevelToStory, StoryDatabase.EVENT_PRE_STORY_SAVE)
@@ -46,23 +51,24 @@ pub.subscribe(mcexamples.algorithms.addSourceNameToStory, StoryDatabase.EVENT_PR
 saved = 0
 first_page = int(config.get('api','first_page'))
 for page in xrange(MAX_PAGES_TO_FETCH):
-  query_page = first_page+page
-  results = mc.allProcessed(query_page)
-  log.info("Fetched "+str(len(results))+" stories (page "+str(query_page)+")")
-  for story in results:
-    worked = db.addStory(story)
-    if worked:
-      saved = saved + 1
-    else:
-      log.warning("  unable to save story "+str(story['stories_id'])) # prob exists already
-  config.set('api','first_page',query_page)
-  with open(CONFIG_FILENAME,'wb') as config_file_handle: 
-    config.write(config_file_handle)
+    query_page = first_page+page
+    results = mc.allProcessed(query_page)
+    log.info("Fetched "+str(len(results))+" stories (page "+str(query_page)+")")
+    for story in results:
+        worked = db.addStory(story)
+        if worked:
+            saved = saved + 1
+        else:
+            log.warning("    unable to save story "+str(story['stories_id'])) # prob exists already
+    config.set('api','first_page',query_page)
+    with open(CONFIG_FILENAME,'wb') as config_file_handle: 
+        config.write(config_file_handle)
 
 max_story_id = db.getMaxStoryId()
 
+# log some performance stats
 end_time = time.time()
 duration = end_time - start_time
 log.info("Saved "+str(saved)+" stories - new max id "+str(max_story_id))
 if saved > 0:
-  log.info("  took "+str(duration)+" secs ("+str(round(duration/saved,2))+" secs per story)")
+    log.info("    took "+str(duration)+" secs ("+str(round(duration/saved,2))+" secs per story)")
